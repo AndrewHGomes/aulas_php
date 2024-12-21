3d3f0c9ec836deea2e77c0dfe6e5165e0c456e88
AndrewHGomes <andrewvianigomes@gmail.com>
2024-12-21 08:54:11 -0300

    Arquivo 19 ficou mais coeso

--- 19-Class.php
+++ 19-Class.php
@@ -2,18 +2,34 @@
 
 class Pessoa
 {
-  public string $nome;
-  public int $idade;
-  public string $genero;
+  public $nome;
+  public $idade;
+  public $genero;
 
-  public function andar(): string
+  public function __construct($nome, $idade, $genero)
   {
-    return 'Eu estou andando';
+    $this->nome = $nome;
+    $this->idade = $idade;
+    $this->genero = $genero;
   }
 
-  public function correr(): string
+  public function andar()
   {
-    return 'Eu estou correndo';
+    return "Está andando";
+  }
+
+  public function correr()
+  {
+    return "Está correndo";
+  }
+
+  public function mostrarDados()
+  {
+    return [
+      'Nome:' => $this->nome,
+      'Idade:' => $this->idade,
+      'Gênero:' => $this->genero,
+    ];
   }
 }
 
@@ -33,56 +49,66 @@ class Pessoa
     <h1>Classes</h1>
   </header>
   <main>
+    <section>
+      <h2>Introdução</h2>
+      <?php
 
-    <h2>Introdução</h2>
+      $pessoa1 = new Pessoa('Andrew', 36, 'Masculino');
 
-    <?php
-
-    $pessoa1 = new Pessoa;
-
-    ?>
-
-    <p>
-      Classe Pessoa: <?= print_r($pessoa1) ?>
-    </p>
-
+      ?>
+      <p>
+        Classe Pessoa: <?= print_r($pessoa1) ?>
+      </p>
+    </section>
     <hr>
+    <section>
+      <h2>Propriedades ou Atributos</h2>
+      <?php
 
-    <h2>Propriedades ou Atributos</h2>
+      $pessoa1->nome = 'Andrew';
+      $pessoa1->idade = 36;
+      $pessoa1->genero = 'Masculino';
 
-    <?php
-
-    $pessoa1->nome = 'Andrew';
-    $pessoa1->idade = 36;
-    $pessoa1->genero = 'Masculino';
-
-    ?>
-
-    <p>
-      Nome: <?= $pessoa1->nome ?>
-    </p>
-    <p>
-      Idade: <?= $pessoa1->idade ?>
-    </p>
-    <p>
-      Gênero: <?= $pessoa1->genero ?>
-    </p>
-
+      ?>
+      <p>
+        Nome: <?= $pessoa1->nome ?>
+      </p>
+      <p>
+        Idade: <?= $pessoa1->idade ?>
+      </p>
+      <p>
+        Gênero: <?= $pessoa1->genero ?>
+      </p>
+    </section>
     <hr>
-
-    <h2>Funções ou Métodos</h2>
-
-    <p>
-      Método andar: <?= $pessoa1->andar() ?>
-    </p>
-    <p>
-      Método correr: <?= $pessoa1->correr() ?>
-    </p>
-
+    <section>
+      <h2>Funções ou Métodos</h2>
+      <p>
+        Método andar: <?= $pessoa1->andar() ?>
+      </p>
+      <p>
+        Método correr: <?= $pessoa1->correr() ?>
+      </p>
+    </section>
     <hr>
+    <section>
+      <h2>$this</h2>
+      <p>
+        Andrew: <?= print_r($pessoa1->mostrarDados()) ?>
+      </p>
+    </section>
+    <hr>
+    <section>
+      <h2>__construct()</h2>
+      <?php
 
-    <h2>This</h2>
+      $pessoa2 = new Pessoa('Viviane', 39, 'Feminino')
 
+      ?>
+      <p>
+        Pessoa2: <?= print_r($pessoa2) ?>
+      </p>
+    </section>
   </main>
 </body>
 
